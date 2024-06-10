dd7fb97b48890e8f3d9b49b21b316e39d341525d
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-06-10 10:42:01 +0200

    step 3 OK

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,28 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  try {
+    // Fetch all boats from the database
+    const affectedRows = await tables.boat.update({
+      id: req.params.id,
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    });
+
+    if (affectedRows === 1) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit,
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+
 const AbstractRepository = require("./AbstractRepository");
 
 class BoatRepository extends AbstractRepository {
@@ -11,6 +13,18 @@ class BoatRepository extends AbstractRepository {
 
     // Return the array of boats
     return rows;
+  }
+
+  async update({ id, coord_x, coord_y }) {
+
+    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
+    const [rows] = await this.database.query(
+      `update ${this.table} set coord_x=?,coord_y=? where id=?`,
+      [coord_x, coord_y, id]
+    );
+
+    // Return the array of boats
+    return rows.affectedRows;
   }
 }
 
