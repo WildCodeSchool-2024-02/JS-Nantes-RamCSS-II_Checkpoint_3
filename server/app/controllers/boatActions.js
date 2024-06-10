7e4b8da10d3757cd705027928c1fd2792b349938
Florine <florinevinet@gmail.com>
2024-06-10 10:30:50 +0200

    step 3 done

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,28 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  try {
+     const body = {
+      id : req.params.id,
+      coordX : req.body.coord_x,
+      coordY : req.body.coord_y
+    }
+
+    const updateBoat = await tables.boat.update(body);
+
+    if (updateBoat === 1) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 module.exports = {
-  browse,
+  browse, edit
 };
--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -12,6 +12,13 @@ class BoatRepository extends AbstractRepository {
     // Return the array of boats
     return rows;
   }
+
+  async update(body) {
+    const {id, coordX, coordY} = body;
+    const [row] = await this.database.query(`update ${this.table} set coord_x = ?, coord_y = ? where id = ?`, [coordX, coordY, id]);
+
+    return row.affectedRows;
+  }
 }
 
 module.exports = BoatRepository;
