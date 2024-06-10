e16681b3877d3558a985fd797d723bf88a33b53d
brewost <156411525+brewost@users.noreply.github.com>
2024-06-10 10:34:13 +0200

    step3 done

--- server/app/controllers/boatActions.js
+++ server/app/controllers/boatActions.js
@@ -13,6 +13,28 @@ const browse = async (req, res, next) => {
   }
 };
 
+const edit = async (req, res, next) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { coord_x, coord_y } = req.body;
+    const { id } = req.params;
+    // eslint-disable-next-line camelcase
+    const updateData = { coord_x, coord_y, id };
+    const affectedRows = await tables.boat.update(updateData);
+
+    if (affectedRows) { // Check if at least one row was affected
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
 module.exports = {
   browse,
+  edit
 };
+
