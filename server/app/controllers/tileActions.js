10e4420d56d387825052caee37377fb4a27e835b
alexis.guillon <alexis.guillon44@orange.fr>
2024-06-10 10:51:33 +0200

    step2 is done

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const TileRepository = require("../../database/models/TileRepository");
+
+const browse = async (req, res, next) => { // eslint-disable-line no-unused-vars
+  try {
+    const tileRepo = new TileRepository(); // Instantiate the TileRepository
+    const tiles = await tileRepo.readAll(); // Use the readAll method
+    res.status(200).json(tiles);
+  } catch (error) {
+    console.error("Error fetching tiles:", error);
+    res.status(500).json({ error: "An error occurred while fetching tiles." });
+  }
+};
+
+module.exports = {
+  browse,
+};
+
+
